af7bcbb51d5a6d16548fd7208bbf075f77eb0f76
Koyonari <yongshyan.an@gmail.com>
2024-04-23 22:13:50 +0800

    Practical 2 + First Challenge

--- app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -1,6 +1,8 @@
 package sg.edu.np.mad.madpractical2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        // Intialize a new User object
+        User user = new User("John Doe", "MAD Developer", 1, false);
+
+        // Get the TextViews and Button from Layout
+        TextView tvName = findViewById(R.id.tvName);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+        Button btnFollow = findViewById(R.id.btnFollow);
+
+        //Onclick listener to change to unfollow
+        btnFollow.setOnClickListener(v -> {
+            if (btnFollow.getText() == "Follow"){
+                btnFollow.setText("Unfollow");
+            }
+            else{
+                btnFollow.setText("Follow");
+            }
+        });
+
+        // Set the TextViews with the User's name, description, default button message
+        tvName.setText(user.name);
+        tvDescription.setText(user.description);
     }
 }
